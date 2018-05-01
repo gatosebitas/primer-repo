bd3d3c337e327603bb1ba0526f9ef53fa61ce121
Fernando <tin22909@gmail.com>
2018-04-30 20:21:52 -0500

    subiendo borrar

---
+++ borrar.cpp
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main(){
+	
+	unsigned  i;
+	for(i=10;i>0;--i)
+		printf("Hola mundo %d ",i);
+		
+	char c = 'Z' +1;
+	
+	printf("/n Hola mundo %c ",c);	
+	return 0;
+}
+
